ef385f3368888cc043339ba9bad287e9a23d3f7b
xzsamar <samarahmed200409@gmail.com>
2026-04-18 00:30:39 +0530

    fixed app.js for deployment

--- server/app.js
+++ server/app.js
@@ -1,14 +1,6 @@
-require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
-require("dotenv").config();
-
-// ✅ Load env variables
-dotenv.config();
-
-
-
 
 const app = express();
 
